90bf2c30c0e2197ec65d3778dde0b09d134f4860
ZiadMedhat33 <ziadmedhat301@gmail.com>
2025-07-22 17:03:49 +0300

    api links

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3833ed5be0ba51fc4e3b54fbe96811e76190583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e2f09073cbf819d6a7225f36d07745b8ad04c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
